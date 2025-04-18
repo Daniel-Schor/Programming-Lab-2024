5708d073dd9384727689ada25b377ec4897930db
Daniel Schor <daniel.schor@gmx.de>
2025-04-18 15:07:05 +0200

    Retry connection build on failed attempt

--- dist/backend/Config/DatabaseConfig.js
+++ dist/backend/Config/DatabaseConfig.js
@@ -8,20 +8,24 @@ const DB_HOST = process.env.DB_HOST; //|| 'localhost';
 const DB_USER = process.env.DB_USER || 'postgres';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'admin';
 const DB_NAME = process.env.DB_NAME || 'pizza';
-const client = new Client({
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_NAME,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-});
-client
-    .connect()
-    .then(() => {
+let client;
+while (true) {
+  try {
+    client = new Client({
+      user: DB_USER,
+      host: DB_HOST,
+      database: DB_NAME,
+      password: DB_PASSWORD,
+      port: DB_PORT,
+    });
+    await client.connect();
     console.log(`✅ Connected to PostgreSQL at ${DB_HOST}:${DB_PORT}`);
-})
-    .catch((err) => {
-    console.error('❌ Error connecting to PostgreSQL database', err);
-});
+    break; // Exit the loop on successful connection
+
+  } catch (err) {
+    console.error(`❌ DB connection failed. Retrying in 3s... (${err.code})`);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+  }
+}
 export default client;
 //# sourceMappingURL=DatabaseConfig.js.map
